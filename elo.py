1c64b687cfbb44219a0c4f5a4dcf52890b7a1a7d
Luca Bennett <74973887+zy18811@users.noreply.github.com>
2021-01-11 15:58:40 +0000

    Add files via upload
    Elo rating for football teams using kaggle db

---
+++ elo.py
@@ -0,0 +1,67 @@
+import sqlite3
+
+con = sqlite3.connect('database.sqlite')
+cur = con.cursor()
+
+cur.execute("UPDATE Team SET elo=1000")
+cur.execute(
+    "SELECT date,home_team_api_id,away_team_api_id,home_team_goal,away_team_goal FROM Match WHERE season != '2015/2016'")
+trainMatches = cur.fetchall()
+trainMatches.sort(key=lambda x: x[0])
+
+
+def eloUpdate(r1, r2, s1, s2, K):  # r1 = team 1 elo, r2 = team 2 elo, s1/2 = 1,0,0.5 depending win/lose/draw
+    R1 = 10 ** (r1 / 400)
+    R2 = 10 ** (r2 / 400)
+
+    e1 = R1 / (R1 + R2)
+    e2 = R2 / (R1 + R2)
+
+    r1_update = r1 + K * (s1 - e1)
+    r2_update = r2 + K * (s2 - e2)
+
+    return r1_update, r2_update
+
+
+for match in trainMatches:
+    if match[3] == match[4]:
+        s1 = s2 = 0.5
+    elif match[3] > match[4]:
+        s1 = 1
+        s2 = 0
+    else:
+        s1 = 0
+        s2 = 1
+    cur.execute("SELECT elo FROM Team where team_api_id = ?", (match[1],))
+    homeTeamElo = cur.fetchall()
+    cur.execute("SELECT elo FROM Team where team_api_id = ?", (match[2],))
+    awayTeamElo = cur.fetchall()
+
+    newElos = eloUpdate(homeTeamElo[0][0], awayTeamElo[0][0], s1, s2, 32)
+    cur.execute("UPDATE Team SET elo = ? where team_api_id = ?", (newElos[0], match[1],))
+    cur.execute("UPDATE Team SET elo = ? where team_api_id = ?", (newElos[1], match[2],))
+
+cur.execute(
+    "SELECT home_team_api_id,away_team_api_id,home_team_goal,away_team_goal FROM Match WHERE season = '2015/2016'")
+testMatches = cur.fetchall()
+
+successCount = 0
+
+for match in testMatches:
+    cur.execute("SELECT elo FROM Team WHERE team_api_id = ?", (match[0],))
+    homeElo = cur.fetchall()
+    cur.execute("SELECT elo FROM Team WHERE team_api_id = ?", (match[1],))
+    awayElo = cur.fetchall()
+    if match[2] > match[3]: # home win
+        if homeElo > awayElo:
+            successCount+=1
+
+    elif match[2] < match[3]: # away win
+        if homeElo<awayElo:
+            successCount+=1
+    elif match[2] == match[3]: # draw
+        if homeElo == awayElo:
+            successCount+=1
+
+accuracy = (successCount * 100) / (len(testMatches))
+print(accuracy)
---
+++ elo2.py
@@ -0,0 +1,152 @@
+import sqlite3
+import numpy as np
+
+con = sqlite3.connect('database.sqlite')
+cur = con.cursor()
+
+cur.execute("UPDATE Team SET elo=1000")
+
+
+def eloUpdate(r1, r2, s1, s2, k,goal1,goal2):  # r1 = team 1 elo, r2 = team 2 elo, s1/2 = 1,0,0.5 depending win/lose/draw
+    if goal1 > goal2:
+        if goal1 - goal2 == 2:
+            k = 1.5*k
+        elif goal1 - goal2 == 3:
+            k = 1.75*k
+        elif goal1 - goal2 >= 4:
+            N = goal1 - goal2
+            k = k * (0.75 + (N-3)/8)
+    elif goal1 < goal2:
+        if goal2 - goal1 == 2:
+            k = 1.5*k
+        elif goal2 - goal1 == 3:
+            k = 1.75*k
+        elif goal2 - goal1 >= 4:
+            N = goal2 - goal1
+            k = k * (0.75 + (N-3)/8)
+
+
+    R1 = 10 ** (r1 / 400)
+    R2 = 10 ** (r2 / 400)
+
+    e1 = R1 / (R1 + R2)
+    e2 = R2 / (R1 + R2)
+
+    r1_update = r1 + k * (s1 - e1)
+    r2_update = r2 + k * (s2 - e2)
+
+    return r1_update, r2_update
+
+
+def percTrainData(i, traindata,k1,k2,k3):
+    if i / len(traindata) > 0.3:
+        return k2
+    if i / len(traindata) > 0.6:
+        return k3
+    return k1
+
+
+def train(k, traindata, z):
+    if z == 1:
+        cur.execute("UPDATE Team SET elo=1000")
+    i = 0
+    for match in traindata:
+        #k = percTrainData(i,traindata,k1,k2,k3)
+        if match[3] == match[4]:
+            s1 = s2 = 0.5
+        elif match[3] > match[4]:
+            s1 = 1
+            s2 = 0
+        else:
+            s1 = 0
+            s2 = 1
+        cur.execute("SELECT elo FROM Team where team_api_id = ?", (match[1],))
+        homeTeamElo = cur.fetchall()
+        cur.execute("SELECT elo FROM Team where team_api_id = ?", (match[2],))
+        awayTeamElo = cur.fetchall()
+
+        newElos = eloUpdate(homeTeamElo[0][0], awayTeamElo[0][0], s1, s2, k,match[3],match[4])
+        cur.execute("UPDATE Team SET elo = ? where team_api_id = ?", (newElos[0], match[1],))
+        cur.execute("UPDATE Team SET elo = ? where team_api_id = ?", (newElos[1], match[2],))
+        i += 1
+
+
+def perDiff(val1, val2):
+    x = abs(val1 - val2)
+    y = (val1 + val2) / 2
+    perDiff = x / y
+    return perDiff
+
+
+def test(testdata, drawThreshold):
+    successCount = 0
+    count = 0
+    predWins = 0
+    predLosses = 0
+    predDraws = 0
+    wins = 0
+    losses = 0
+    draws = 0
+    for match in testdata:
+
+        cur.execute("SELECT elo FROM Team WHERE team_api_id = ?", (match[1],))
+        homeElo = cur.fetchall()[0][0]
+        cur.execute("SELECT elo FROM Team WHERE team_api_id = ?", (match[2],))
+        awayElo = cur.fetchall()[0][0]
+
+        if abs(homeElo - awayElo) < drawThreshold:
+            predDraws += 1
+            pred = 0
+        elif homeElo > awayElo:
+            predWins += 1
+            pred = 1
+        elif homeElo < awayElo:
+            predLosses += 1
+            pred = -1
+
+        if match[3] > match[4]:  # home win
+            count += 1
+            wins += 1
+            if pred == 1:
+                successCount += 1
+        elif match[3] < match[4]:  # away win
+            count += 1
+            losses += 1
+            if pred == -1:
+                successCount += 1
+        elif match[3] == match[4]:  # draw
+            count += 1
+            draws += 1
+            if pred == 0:
+                successCount += 1
+
+    accuracy = (successCount * 100) / count
+
+    return accuracy
+
+
+cur.execute(
+    "SELECT date,home_team_api_id,away_team_api_id,home_team_goal,away_team_goal FROM Match WHERE id <25000")
+trainMatches = cur.fetchall()
+trainMatches.sort(key=lambda x: x[0])
+
+cur.execute(
+    "SELECT date,home_team_api_id,away_team_api_id,home_team_goal,away_team_goal FROM Match WHERE id > 25000")
+testMatches = cur.fetchall()
+
+train(32, trainMatches, 1)
+
+cur.execute("SELECT elo FROM Team")
+teamElos = cur.fetchall()
+# print(teamElos)
+
+cur.execute("SELECT MAX(elo) FROM Team")
+maxElo = cur.fetchall()
+
+cur.execute("SELECT team_long_name FROM Team WHERE elo = ?", (maxElo[0][0],))
+maxEloTeam = cur.fetchall()
+
+acc = test(testMatches,30)
+
+print("Accuracy = " + str(acc))
+
